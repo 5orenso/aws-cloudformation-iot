421fbde54386393066ff696a50cf92bd7ab2fa4c
Oistein Sorensen <sorenso@gmail.com>
2017-02-15 10:20:28 +0100

    Initial commit

---
+++ bin/set-env-dist.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+export API_NAME='hello-world'
+export API_PATH='hello-world'
+# Use https://github.com/5orenso/aws-lambda-boilerplate to generate the zip-file.
+export LAMBDA_CODE_BUCKET='aws-lambda-autodeploy-lambda'
+export LAMBDA_CODE_KEY='aws-lambda-boilerplate.zip'
